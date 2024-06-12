b384eb80ecf0184033b726daa4acfe256239465a
imane <bellaghma.imane@gmail.com>
2024-06-12 15:24:51 +0100

    added2

--- ass1.php
+++ ass1.php
@@ -1,6 +1,9 @@
 <?php
 
 $var=rand(10,20);
+while ($var == 10 || $var == 20) {
+$var=rand(10,20);
+}
 print_r($var);
 
 
--- ass2.php
+++ ass2.php
@@ -1,8 +1,26 @@
 <?php
+/*
 $friends = ["Osama", "Ahmed", "Sayed", "Ibrahim"];
 
-//rand($friends);
-print_r($frindes);
+$index=  array_rand($friends);
+
+echo $frindes[$index];*/
+
+
+
+
+// Liste des amis
+$friends = ["Osama", "Ahmed", "Sayed", "Ibrahim"];
+
+// Sélection d'un indice aléatoire de l'array
+$random_index = array_rand($friends);
+
+// Affichage de l'ami correspondant à l'indice aléatoire
+echo $friends[$random_index];
+
+?>
+
 // Example
 // Ibrahim
 // Ahmed
+?>
